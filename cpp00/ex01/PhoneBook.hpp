e7243ff78d83014232b39ca8c9fbd443688a2732
hnoguchi <>
2023-04-21 10:30:45 +0900

    [cpp00/ex01/PhoneBook.hpp] Create file.

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 10:13:33 by hnoguchi          #+#    #+#             */
+/*   Updated: 2023/04/21 10:29:57 by hnoguchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+# define MAX_CONTACTS 8
+# define SIZE_OF_ARRAY(array) sizeof(array)/sizeof(array[0]);
+
+class PhoneBook {
+
+
+
+}
+
+#endif
