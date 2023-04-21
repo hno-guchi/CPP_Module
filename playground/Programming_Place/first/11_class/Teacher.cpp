f3dd3cffe74d68d78a9a183d8df8dbc91f6d6bd8
hnoguchi <>
2023-04-21 10:11:10 +0900

    [11_class] Finish Practice.

--- playground/Programming_Place/first/11_class/Teacher.cpp
+++ playground/Programming_Place/first/11_class/Teacher.cpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:46:18 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/20 19:20:38 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/21 10:07:58 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,3 +41,34 @@ void	Teacher::PrintMyStudents()
 		m_StudentList[i]->PrintMember();
 	}
 }
+
+/*
+void	Teacher::SetData(std::string name, int grade)
+{
+	mName = name;
+	mGrade = grade;
+	for (int i = 0; i < SIZE_OF_ARRAY(mChargeStudents); i++)
+	{
+		mChargeStudents[i] = NULL;
+	}
+	mChargeStudentsCount = 0;
+}
+
+void	Teacher::AddChargeStudent(Student *student)
+{
+	if (SIZE_OF_ARRAY(mChargeStudents) < mChargeStudentsCount)
+	{
+		return ;
+	}
+	if (student->GetGrade() != mGrade)
+	{
+		return ;
+	}
+	if (student == NULL)
+	{
+		return ;
+	}
+	mChargeStudents[mChargeStudentsCount] = student;
+	mChargeStudentsCount += 1;
+}
+*/
--- playground/Programming_Place/first/11_class/Teacher.hpp
+++ playground/Programming_Place/first/11_class/Teacher.hpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:35:57 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/20 19:27:34 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/21 10:08:00 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include "Student.hpp"
 
+// MY
 class Teacher {
 	std::string	m_Name;
 	int			m_Grade;
@@ -28,5 +29,24 @@ public:
 	void	AddStudent(Student *s);
 	void	PrintMyStudents();
 };
+
+// 担当できる生徒の最大数
+/*
+# define CHARGE_STUDENT_MAX 40
+# define SIZE_OF_ARRAY(array) sizeof(array)/sizeof(array[0])
+
+class Teacher {
+	std::string	mName;
+	int			mGrade;
+	Student		*mChargeStudents[CHARGE_STUDENT_MAX];
+	int			mChargeStudentsCount;
+
+public:
+	void	SetData(std::string name, int grade);
+	int		GetGrade();
+	void	AddChargeStudent(Student *student);
+	void	PrintChargeStudentsList();
+}
+*/
 
 #endif
