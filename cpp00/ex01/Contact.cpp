5d35de19c3e22abe2f4e713beb66f5ff9014003c
hnoguchi <>
2023-04-25 12:38:16 +0900

    Fix conflict

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 10:13:33 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/25 12:24:18 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:23:52 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CONTACT_HPP
-# define CONTACT_HPP
+#include "Contact.hpp"
 
-#include <iostream>
-
+/*
 class Contact {
 	int			m_index;
 	std::string	m_first_name;
@@ -26,7 +24,35 @@ class Contact {
 public:
 	void	print_data();
 	void	add_data(int idx, std::string first, std::string last,
-				std::string nick, std::string phone_num, std::string secret);
-};
+		std::string nick, std::string phone_num, std::string secret);
+}
+*/
 
-#endif
+void	Contact::print_data()
+{
+	std::cout
+		<< m_index << " | "
+		<< m_first_name << " | "
+		<< m_last_name << " | "
+		<< m_nickname
+		<< std::endl;
+}
+
+void	Contact::add_data(int idx, std::string first, std::string last,
+			std::string nick, std::string phone_num, std::string secret)
+{
+	m_index = idx;
+	m_first_name = first;
+	m_last_name = last;
+	m_nickname = nick;
+	m_phone_number = phone_num;
+	m_darkest_secret = secret;
+}
+
+int	main()
+{
+	Contact	contact;
+
+	contact.add_data(0, "Hidenobu", "Noguchi", "hnoguchi", "012-3456-7890", "SECRET");
+	contact.print_data();
+}
