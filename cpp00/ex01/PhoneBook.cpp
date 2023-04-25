5728660fe4712fc68bb9f1972b26bf8d95114ff2
hnoguchi <>
2023-04-25 12:28:46 +0900

    [ex01] Creat add_data(); and print_data(); member func in Contact class.

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,18 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 10:13:33 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/25 12:10:08 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:09:57 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
-
+# include "PhoneBook.hpp"
+/*
 #include "Contact.hpp"
 
 # define MAX_CONTACTS 8
@@ -27,5 +26,4 @@ public:
 	void	search_command();
 	void	exit_command();
 }
-
-#endif
+*/
