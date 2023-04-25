7e77521d2fd9b35be9de5699f7480303aa9cf4ea
hnoguchi <>
2023-04-25 11:17:56 +0900

    [ex01] Create file. Contact.hpp

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 10:13:33 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/25 11:17:19 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/25 11:16:09 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
-#include "Contact.hpp"
-
-# define MAX_CONTACTS 8
-# define SIZE_OF_ARRAY(array) sizeof(array)/sizeof(array[0]);
-
-class PhoneBook {
-	Contact	contact_list[MAX_CONTACTS];
+class Contact {
+	int			index;
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
 
 public:
-	void	initialize();
-	void	add_command();
-	void	search_command();
-	void	exit_command();
+	print_data();
 }
 
 #endif
