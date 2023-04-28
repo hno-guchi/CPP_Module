ca540a36c43ea2c659e822af27410334eb7e4646
hnoguchi <>
2023-04-28 09:45:31 +0900

    [ex00] Fix. Not use toupper

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,38 +6,42 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 14:18:31 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/04/13 19:08:21 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/04/28 09:45:06 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
 #include <string>
 
-static void	str_toupper(std::string *str)
+char	ft_toupper(char c)
 {
-	const std::string::const_iterator	iter_end = str->end();
-	for (std::string::iterator iter = str->begin(); iter != iter_end; iter++)
-	{
-		*iter = std::toupper(*iter);
+	if ('a' <= c && c <= 'z') {
+		return ('A' + (c - 'a'));
 	}
+	return (c);
+}
+
+static std::string	str_toupper(char *str)
+{
+	std::string	uppered_str;
+
+	for (int i = 0; str[i] != '\0'; i++) {
+		uppered_str += ft_toupper(str[i]);
+	}
+	return (uppered_str);
 }
 
 int	main(int argc, char *argv[])
 {
 	std::string	error_message("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	std::string	str;
 
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cerr << error_message << std::endl;
 		return (1);
 	}
-
-	std::string	str;
-	for (int i = 1; i < argc; i++)
-	{
-		str = argv[i];
-		str_toupper(&str);
+	for (int i = 1; i < argc; i++) {
+		str = str_toupper(argv[i]);
 		std::cout << str << std::flush;
 	}
 	std::cout << std::endl;
