daf6d18d55d5727d871c53739f2c7aae72d64772
hnoguchi <>
2023-05-12 12:21:10 +0900

    [cpp02/ex00] Study copy.

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,11 +6,24 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:53:53 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/05/10 16:54:16 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:24:53 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(const Fixed& src);
+		~Fixed();
+
+		Fixed& operator=(const Fixed& rhs);
+
+
+
+	private:
+};
 
 #endif
