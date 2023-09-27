4e25493b0b40baf62b1d2fa1a0535e0c7777af4b
hnoguchi <>
2023-09-27 14:12:08 +0900

    Programming_Place begin 09_map_and_multimap

---
+++ playground/Programming_Place/standard_library/09_map_and_multimap/map.cpp
@@ -0,0 +1,13 @@
+#if 0
+
+map と multimap | Programming Place Plus　C++編【標準ライブラリ】　第９章
+https://programming-place.net/ppp/contents/cpp/library/009.html#
+
+STLコンテナの１種。
+ソートされた集合を提供する。
+mapはキーの重複を許さないが、multimapはキーの重複を許す。
+連想配列
+mapのキーは、原則変更できない。
+
+
+#endif
