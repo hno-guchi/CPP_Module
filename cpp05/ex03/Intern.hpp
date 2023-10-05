d95a29bb0f83933e625cf290b7c4b2efb2d93a2a
Hidenobuk <>
2023-10-05 17:01:19 +0900

    [cpp05/ex03] テストの作成。（main.cpp）

--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -6,39 +6,56 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/05 15:27:12 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/05 16:44:23 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERN_HPP
 # define INTERN_HPP
 
+#include <iostream>
 #include "debugMessage.hpp"
 #include "color.hpp"
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+#define NOT_EXIST_FORM_MESSAGE "Not exist form."
+
+class AForm;
+class PresidentialPardonForm;
+class RobotomyRequestForm;
+class ShrubberyCreationForm;
 
 class Intern {
 private:
 	// SUBJECT ATTRIBUTE
-
 	// MY ATTRIBUTE
-
 public:
 	// CONSTRUCTER
 	Intern();
-	Intern(const std::string& type);
 	Intern(const Intern& src);
 	// DESTRUCTER
 	~Intern();
-
 	// OPERATOR
 	Intern&	operator=(const Intern& rhs);
-
 	// GETTER
 
 	// SETTER
 
 	// SUBJECT FUNC
-
+	AForm*	makeForm(const std::string& name, const std::string& target);
+	// EXCEPTION
+	class NotExistFormException : public std::exception {
+	private:
+		std::string	message_;
+	
+	public:
+		NotExistFormException() throw();
+		~NotExistFormException() throw();
+		const char*	what() const throw();
+	};
 };
 
 #endif
