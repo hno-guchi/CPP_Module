423777db4ee0689c59166bf8db6a64d41af2fadf
hno-guchi <>
2023-10-13 18:30:01 +0900

    [playground] std::stodの挙動確認

--- playground/cpp06/stod_sample.cpp
+++ playground/cpp06/stod_sample.cpp
@@ -95,4 +95,59 @@ int	main()
 	catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
+
+	str = "-2147483648";
+	std::cout << "std::stod(" << str << "); => " << std::flush;
+	try {
+		d = std::stod(str);
+		std::cout << d << std::endl;
+		std::cout << "INT : " << static_cast<int>(d) << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	str = "-2147483649";
+	std::cout << "std::stod(" << str << "); => " << std::flush;
+	try {
+		d = std::stod(str);
+		std::cout << d << std::endl;
+		std::cout << "INT : " << static_cast<int>(d) << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	str = "2147483647";
+	std::cout << "std::stod(" << str << "); => " << std::flush;
+	try {
+		d = std::stod(str);
+		std::cout << d << std::endl;
+		std::cout << "INT : " << static_cast<int>(d) << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	str = "2147483648";
+	std::cout << "std::stod(" << str << "); => " << std::flush;
+	try {
+		d = std::stod(str);
+		std::cout << d << std::endl;
+		std::cout << "INT : " << static_cast<int>(d) << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	str = "2147483647000000000000";
+	std::cout << "std::stod(" << str << "); => " << std::flush;
+	try {
+		d = std::stod(str);
+		std::cout << d << std::endl;
+		std::cout << "INT : " << static_cast<int>(d) << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
