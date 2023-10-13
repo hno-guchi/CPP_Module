b32d35814a2a38463b497adb64b4ed3c4b6eba55
hnoguchi <>
2023-10-13 11:03:12 +0900

    [cpp06/ex00] ScalarConverter::convertメンバ関数の実装。

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/12 17:44:46 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/13 09:22:33 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include "debugMessage.hpp"
 #include "color.hpp"
 #include <iostream>
+#include <iomanip>
 #include <locale>
 #include <string>
 
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,15 +6,15 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 13:46:07 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/12 18:10:58 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/13 09:24:46 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "ScalarConverter.hpp"
+#include "ScalarConverter.hpp"
 #include "color.hpp"
-#include <string>
-#include <iostream>
-#include <iomanip>
+// #include <string>
+// #include <iostream>
+// #include <iomanip>
 
 int	main(int argc, char **argv)
 {
@@ -23,17 +23,16 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	std::string	str = argv[1];
-	// ScalarConverter::convert(static_cast<std::string&>(argv[1]));
-	// ScalarConverter::convert(str);
-	std::cout << std::stoi(str) << std::endl;
+	ScalarConverter::convert(str);
+	// std::cout << std::stoi(str) << std::endl;
 
-	std::cout << std::endl;
-	std::cout << GREEN << "++++++++++ float manip ++++++++++" << END << std::endl;
-	std::cout << "std::fixed << std::setprecision(1) << std::stof(str) : [" << std::fixed << std::setprecision(1) << std::stof(str) << "]" << std::endl;
-	std::cout << "std::fixed << std::hexfloat << std::stof(str)        : [" << std::fixed << std::hexfloat << std::stof(str) << "]" << std::endl;
-	std::cout << "std::fixed << std::defaultfloat << std::stof(str)    : [" << std::fixed <<  std::defaultfloat << std::stof(str) << "]" << std::endl;
+	// std::cout << std::endl;
+	// std::cout << GREEN << "++++++++++ float manip ++++++++++" << END << std::endl;
+	// std::cout << "std::fixed << std::setprecision(1) << std::stof(str) : [" << std::fixed << std::setprecision(1) << std::stof(str) << "]" << std::endl;
+	// std::cout << "std::fixed << std::hexfloat << std::stof(str)        : [" << std::fixed << std::hexfloat << std::stof(str) << "]" << std::endl;
+	// std::cout << "std::fixed << std::defaultfloat << std::stof(str)    : [" << std::fixed <<  std::defaultfloat << std::stof(str) << "]" << std::endl;
 
-	std::cout << std::endl;
-	std::cout << GREEN << "++++++++++ double manip ++++++++++" << END << std::endl;
-	std::cout << std::fixed << std::setprecision(1) << std::stod(str) << std::endl;
+	// std::cout << std::endl;
+	// std::cout << GREEN << "++++++++++ double manip ++++++++++" << END << std::endl;
+	// std::cout << std::fixed << std::setprecision(1) << std::stod(str) << std::endl;
 }
