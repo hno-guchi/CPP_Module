8937bc21a07e024c9ad88ff2fd15e9a85eaf839f
hno-guchi <>
2023-10-17 17:30:31 +0900

    [cpp06/ex00] 境界値検証テストの実装（main.cpp）

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 13:46:07 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/17 10:28:08 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:29:17 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,28 +52,52 @@ int	main()
 	str = "255";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "-2147483649";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "-2147483648";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "2147483647";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "2147483648";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "-3.40282e+38";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "-3.40283e+38";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "1.17549e-38";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "1.17550e-38";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "3.40282e+38";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "3.40283e+38";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "-1.79769e+308";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "-1.79770e+308";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "2.22507e-308";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "2.22508e-308";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
 	str = "1.79769e+308";
+	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
+	ScalarConverter::convert(str); std::cout << std::endl;
+	str = "1.79770e+308";
 	std::cout << GREEN << "========== [" << END << str << GREEN << "] ==========" << END << std::endl;
 	ScalarConverter::convert(str); std::cout << std::endl;
 
