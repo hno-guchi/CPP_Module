a1588297d6e9fed96ba6d2077fac0fd1b3bf3547
hno-guchi <>
2023-10-18 13:54:26 +0900

    [cpp06/ex01] Delete dir cpp06_2

---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 13:46:07 by hnoguchi          #+#    #+#             */
+/*   Updated: 2023/10/18 13:53:49 by hnoguchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+#include "color.hpp"
+
+int	main()
+{
+	Data		data;
+	uintptr_t	uint = Serializer::serialize(&data);
+	Data		*ptr = Serializer::deserialize(uint);
+
+	std::cout << GREEN << "++++++++++++++++++++++++++++++++++++++++" << END << std::endl;
+	std::cout << "        Data ptr : [" << &data << "]" << std::endl;
+	std::cout << "deserialized ptr : [" << ptr << "]" << std::endl;
+
+	std::cout << std::endl;
+
+	uint = Serializer::serialize(NULL);
+	ptr = Serializer::deserialize(uint);
+	std::cout << GREEN << "++++++++++++++++++++++++++++++++++++++++" << END << std::endl;
+	std::cout << "        Data ptr : [" << NULL << "]" << std::endl;
+	std::cout << "deserialized ptr : [" << ptr << "]" << std::endl;
+
+	// Compile error
+	// int	*iptr;
+	// uint = Serializer::serialize(iptr);
+}
