18fc1502afb2e5ff3532e54fabfa23440f5c4aa7
hno-guchi <>
2023-10-19 15:06:06 +0900

    [cpp07/ex00]Subjectにあるテストを追加

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:29:55 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/19 14:58:46 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/19 15:04:19 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,6 +125,29 @@ int	main()
 	std::cout << "result : [" << result << "]" << std::endl;
 	std::cout << "       : [" << &result << "]" << std::endl;
 	std::cout << std::endl;
+}
+
+#elif SUB
+
+int	main(void)
+{
+	int	a = 2;
+	int	b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return (0);
 }
 
 #else
