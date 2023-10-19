a02ebf4adbd68ece32909f3778fdb9540ef4a6b3
hnoguchi <>
2023-10-19 10:58:05 +0900

    [cpp07/ex00] whatever.cppを作成

--- cpp07/ex00/whatever.cpp
+++ cpp07/ex00/whatever.cpp
@@ -1,20 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   whatever.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnoguchi <hnoguchi@42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:13:09 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/19 10:57:32 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/19 10:57:33 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#include "whatever.hpp"
 
 template <typename T>
-void	swap(T& a, T& b);
+void	swap(T& a, T& b)
+{
+	if (&a == &b) {
+		return ;
+	}
+	T	temp = a;
+	a = b;
+	b = temp;
+}
 
 // template <typename T>
 // T	min(T a, T b);
@@ -22,4 +29,3 @@ void	swap(T& a, T& b);
 // template <typename T>
 // T	max(T a, T b);
 
-#endif
