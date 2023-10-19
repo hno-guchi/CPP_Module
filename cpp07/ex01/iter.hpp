3da50f2e031d73bba97b9fc11d2090c76cbdfd37
hno-guchi <>
2023-10-19 18:02:52 +0900

    [cpp07/ex01]iterテンプレート関数の実装（const対応ver）

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 15:08:01 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/19 16:04:41 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/19 18:00:37 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,36 @@
 
 #include <iostream>
 
-template <typename T1, typename T2, typename T3>
-void	iter(T1* addr, T2 size, T3 (*func)(T1*))
+template <typename T1, typename T2>
+void	iter(T1* addr, const std::size_t size, T2 (*func)(T1*))
 {
 	if (addr == NULL || func == NULL) {
 		return ;
 	}
-	for (std::size_t i = 0; i < static_cast<size_t>(size); i++) {
+	for (std::size_t i = 0; i < size; i++) {
+		(void)func(&(addr[i]));
+	}
+}
+
+template <typename T1, typename T2>
+void	iter(T1 const* addr, const std::size_t size, T2 (*func)(T1 const*))
+{
+	if (addr == NULL || func == NULL) {
+		return ;
+	}
+	for (std::size_t i = 0; i < size; i++) {
 		func(&(addr[i]));
 	}
+}
+
+template <typename T>
+void	PrintAryData(T* ary, std::size_t size)
+{
+	std::cout << "value : " << std::flush;
+	for (std::size_t i = 0; i < size; i++) {
+		std::cout << " ["<< ary[i] << "] " << std::flush;
+	}
+	std::cout << std::endl;
 }
 
 #endif
