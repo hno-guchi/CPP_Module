3f69963280bdbc3407d9ca6749521b19dcebda67
hnoguchi <>
2023-10-25 14:13:06 +0900

    [cpp07/ex02] subjectのテストを追加

--- cpp07/ex02/Array.tpp
+++ cpp07/ex02/Array.tpp
@@ -19,8 +19,7 @@ Array<T>::Array() :
 {
 	debugMessage("Array", DEFAULT_CONSTRUCT);
 	try {
-		this->data_ = new T();
-		this->data_[0] = 0;
+		this->data_ = new T[1]();
 	}
 	catch (std::exception& e) {
 		std::cerr << RED << e.what() << END << std::endl;
@@ -30,14 +29,11 @@ Array<T>::Array() :
 
 template <typename T>
 Array<T>::Array(const unsigned int n) :
-	size_(static_cast<std::size_t>(n)), data_(NULL)
+	size_(n), data_(NULL)
 {
 	debugMessage("Array", HAS_ARG_CONSTRUCT);
 	try {
-		this->data_ = new T[n];
-		for (std::size_t i = 0; i < n; i++) {
-			this->data_[i] = 0;
-		}
+		this->data_ = new T[n]();
 	}
 	catch (std::exception& e) {
 		std::cerr << RED << e.what() << END << std::endl;
@@ -46,7 +42,8 @@ Array<T>::Array(const unsigned int n) :
 }
 
 template <typename T>
-Array<T>::Array(const Array& src)
+Array<T>::Array(const Array& src) :
+	size_(src.size()), data_(NULL)
 {
 	debugMessage("Array", COPY_CONSTRUCT);
 	this->operator=(src);
@@ -62,7 +59,7 @@ Array<T>&	Array<T>::operator=(const Array& rhs)
 	}
 	try {
 		delete [] this->data_;
-		this->data_ = new T[rhs.size()];
+		this->data_ = new T[rhs.size()]();
 		for (std::size_t i = 0; i < rhs.size(); i++) {
 			this->data_[i] = rhs.data_[i];
 		}
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,15 @@
 /*   By: hnoguchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 17:49:32 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/10/24 19:25:37 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:08:55 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Array.hpp"
 #include <iostream>
+#include "Array.hpp"
+#include "color.hpp"
+
+#ifndef SUB_TEST
 
 int	main()
 {
@@ -32,4 +35,54 @@ int	main()
 #ifdef LEAKS
 	system("leaks -q ex02");
 #endif
+	return (0);
 }
+
+#else
+
+#define MAX_VAL 750
+int main()
+{
+	Array<int>	numbers(MAX_VAL);
+	int*		mirror = new int[MAX_VAL];
+
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++) {
+		const int	value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int>	tmp = numbers;
+		Array<int>	test(tmp);
+	}
+	for (int i = 0; i < MAX_VAL; i++) {
+		if (mirror[i] != numbers[i]) {
+			std::cerr << RED << "didn't save the same value!!" << END << std::endl;
+			return (1);
+		}
+	}
+	try {
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e) {
+		std::cerr << RED << e.what() << END << std::endl;
+	}
+	try {
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e) {
+		std::cerr << RED << e.what() << END << std::endl;
+	}
+	for (int i = 0; i < MAX_VAL; i++) {
+		numbers[i] = rand();
+	}
+	delete [] mirror;
+#ifdef LEAKS
+	system("leaks -q ex02");
+#endif // LEAKS
+	return (0);
+}
+
+#endif // SUB_TEST
