e6d25ce6f69908b1b9aa83faa4bfd0397bb36c23
hnoguchi <>
2023-10-31 18:27:29 +0900

    [cpp08/ex02]MutantStackテンプレートクラスの実装を開始。

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
+/*   Updated: 2023/10/31 18:26:51 by hnoguchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+private:
+	// SUBJECT ATTRIBUTE
+
+	// MY ATTRIBUTE
+
+public:
+	// CONSTRUCTOR
+	// MutantStack();
+	// MutantStack(const std::string& type);
+	// MutantStack(const MutantStack& src);
+	// DESTRUCTOR
+	// ~MutantStack();
+
+	// OPERATOR
+	// MutantStack&	operator=(const MutantStack& rhs);
+
+	// GETTER
+
+	// SETTER
+
+	// SUBJECT FUNC
+
+};
+
+#endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 18:31:58 by hnoguchi          #+#    #+#             */
+/*   Updated: 2023/10/31 18:25:55 by hnoguchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "color.hpp"
+#include "MutantStack.hpp"
+
+#ifdef TEST
+
+#else // TEST
+
+int main()
+{
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	// MutantStack<int>::iterator it = mstack.begin();
+	// MutantStack<int>::iterator ite = mstack.end();
+
+	// ++it;
+	// --it;
+	// while (it != ite)
+	// {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+	std::stack<int> s(mstack);
+#ifdef LEAKS
+	system("leaks -q ex01");
+#endif
+	return (0);
+}
+
+#endif // TEST
