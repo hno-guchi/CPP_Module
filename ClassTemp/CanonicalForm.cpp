f08f7b68a42bb9552814d605cf1216a61f53f115
hno-guchi <>
2023-11-08 14:58:13 +0900

    [cpp09/ex01]RPNクラス。コンストラクタの実装。

--- ClassTemp/CanonicalForm.cpp
+++ ClassTemp/CanonicalForm.cpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/11/08 10:26:01 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/11/08 14:34:05 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,32 @@
 // CONSTRUCTER
 CanonicalForm::CanonicalForm()
 {
+#ifdef DEBUG
 	debugMessage("CanonicalForm", DEFAULT_CONSTRUCT);
+#endif // DEBUG
 }
 
 CanonicalForm::CanonicalForm() :
 {
+#ifdef DEBUG
 	debugMessage("CanonicalForm", HAS_ARG_CONSTRUCT);
+#endif // DEBUG
 }
 
 CanonicalForm::CanonicalForm(const CanonicalForm& src)
 {
+#ifdef DEBUG
 	debugMessage("CanonicalForm", COPY_CONSTRUCT);
+#endif // DEBUG
 	this->operator=(src);
 }
 
 // DESTRUCTER
 CanonicalForm::~CanonicalForm()
 {
+#ifdef DEBUG
 	debugMessage("CanonicalForm", DESTRUCT);
+#endif // DEBUG
 }
 
 // OPERATOR
@@ -41,7 +49,9 @@ CanonicalForm&	CanonicalForm::operator=(const CanonicalForm& rhs)
 	if (this == &rhs) {
 		return (*this);
 	}
+#ifdef DEBUG
 	debugMessage("CanonicalForm", COPY_OPERATOR);
+#endif // DEBUG
 	return (*this);
 }
 
