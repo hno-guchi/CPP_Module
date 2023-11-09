b7dce684eba1ac4059f2fd3e6266d4997223418a
hno-guchi <>
2023-11-09 16:27:31 +0900

    [cpp09/ex01] RPNクラス。executeメンバ関数の実装。validation部分の実装。

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,12 +6,14 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/11/08 18:39:53 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/11/09 16:26:14 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include "debugMessage.hpp"
+
+const std::string	RPN::operations_ = "+-/*";
 
 // CONSTRUCTER
 RPN::RPN()
@@ -29,6 +31,29 @@ RPN::~RPN()
 #endif // DEBUG
 }
 
+static void	printBuff(const std::deque<int>& buf)
+{
+	if (buf.empty()) {
+		return ;
+	}
+	int	i = 0;
+	for (std::deque<int>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
+		std::cout << "[" << i << "]" << std::flush;
+		if ((itr + 1) != buf.end()) {
+			std::cout << " -> " << std::flush;
+		}
+		i += 1;
+	}
+	std:: cout << std::endl;
+	for (std::deque<int>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
+		std::cout << "[" << *itr << "]" << std::flush;
+		if ((itr + 1) != buf.end()) {
+			std::cout << " -> " << std::flush;
+		}
+	}
+	std:: cout << std::endl;
+}
+
 // SUBJECT FUNC
 void	RPN::execute(std::string str) const
 {
@@ -36,10 +61,11 @@ void	RPN::execute(std::string str) const
 		throw RPN::InvalidArg();
 		return ;
 	}
-	std::locale	l = std::locale::classic();
+	std::deque<int>	buff;
+	std::locale		l = std::locale::classic();
 	for (std::string::iterator itr = str.begin(); itr != str.end(); itr++) {
 		if (!std::isspace(*itr, l)) {
-			std::string				token; 
+			std::string	token; 
 			// getToken(*itr, token);
 			std::string::iterator	tokenItr = itr;
 			for (; !std::isspace(*tokenItr, l); tokenItr++) {
@@ -53,16 +79,70 @@ void	RPN::execute(std::string str) const
 				throw RPN::InvalidArg();
 				// return ;
 			}
-			// if (std::isdigit(static_cast<char>(token[0]), l)) {
-			if (std::isdigit(token[0], l)) {
-				std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [digit]" << std::endl;
+			if (std::isdigit(static_cast<char>(token[0]), l)) {
+				switch (static_cast<char>(token[0])) {
+					case '0':
+						buff.push_front(0);
+						break;
+					case '1':
+						buff.push_front(1);
+						break;
+					case '2':
+						buff.push_front(2);
+						break;
+					case '3':
+						buff.push_front(3);
+						break;
+					case '4':
+						buff.push_front(4);
+						break;
+					case '5':
+						buff.push_front(5);
+						break;
+					case '6':
+						buff.push_front(6);
+						break;
+					case '7':
+						buff.push_front(7);
+						break;
+					case '8':
+						buff.push_front(8);
+						break;
+					case '9':
+						buff.push_front(9);
+						break;
+					default:
+						throw RPN::InvalidArg();
+						return ;
+				}
+			}
+			else if (this->operations_.find(token) != this->operations_.npos) {
+				switch (static_cast<char>(token[0])) {
+					case '+':
+						std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [operator]" << std::endl;
+						break;
+					case '-':
+						std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [operator]" << std::endl;
+						break;
+					case '/':
+						std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [operator]" << std::endl;
+						break;
+					case '*':
+						std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [operator]" << std::endl;
+						break;
+					default:
+						throw RPN::InvalidArg();
+						return ;
+				}
 			}
 			else {
-				std::cout << "token: [" << token << "] | size: [" << token.size() << "] | type: [operator]" << std::endl;
+				throw RPN::InvalidArg();
+				return ;
 			}
 			itr = tokenItr;
 		}
 	}
+	printBuff(buff);
 }
 
 // EXCEPTION
