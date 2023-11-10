2ceccc2439be5a7afd905a16ea0b62bfea527ad3
hno-guchi <>
2023-11-10 17:56:16 +0900

    [cpp09/ex01] RPNクラス。executeメンバ関数の実装。が完了。

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,12 +6,15 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:47:02 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/11/10 14:26:41 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:47:17 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include "debugMessage.hpp"
+
+// debug
+// static void	printBuff(const std::deque<long>& buf);
 
 const std::string	RPN::operations_ = "+-/*";
 
@@ -31,67 +34,103 @@ RPN::~RPN()
 #endif // DEBUG
 }
 
-static void	printBuff(const std::deque<int>& buf)
+static void	multiplication(std::deque<long>& buf)
 {
-	if (buf.empty()) {
-		return ;
+	long	b = buf.front();
+	buf.pop_front();
+	long	a = buf.front();
+	buf.pop_front();
+	long	result = a * b;
+	if (result < INT_MIN || INT_MAX < result) {
+		throw RPN::OverInt();
 	}
-	int	i = 0;
-	for (std::deque<int>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
-		std::cout << "[" << i << "]" << std::flush;
-		if ((itr + 1) != buf.end()) {
-			std::cout << " -> " << std::flush;
+	buf.push_front(result);
+}
+
+static void	division(std::deque<long>& buf)
+{
+	long	b = buf.front();
+	buf.pop_front();
+	long	a = buf.front();
+	buf.pop_front();
+	if (b == 0) {
+		throw RPN::InvalidArg("Occurs Zero division.");
+	}
+	long	result = a / b;
+	if (result < INT_MIN || INT_MAX < result) {
+		throw RPN::OverInt();
+	}
+	buf.push_front(result);
+}
+
+static void	subtraction(std::deque<long>& buf)
+{
+	long	b = buf.front();
+	buf.pop_front();
+	long	a = buf.front();
+	buf.pop_front();
+	long	result = a - b;
+	if (result < INT_MIN || INT_MAX < result) {
+		throw RPN::OverInt();
+	}
+	buf.push_front(result);
+}
+
+static void	addition(std::deque<long>& buf)
+{
+	long	b = buf.front();
+	buf.pop_front();
+	long	a = buf.front();
+	buf.pop_front();
+	long	result = a + b;
+	if (result < INT_MIN || INT_MAX < result) {
+		throw RPN::OverInt();
+	}
+	buf.push_front(result);
+}
+
+static void	calculate(const char operation, std::deque<long>& buff)
+{
+	if (buff.size() < 2) {
+		throw RPN::InvalidArg("Not enough numbers.");
+	}
+	try {
+		switch (operation) {
+			case '+':
+				addition(buff);
+				break;
+			case '-':
+				subtraction(buff);
+				break;
+			case '/':
+				division(buff);
+				break;
+			case '*':
+				multiplication(buff);
+				break;
+			default:
+				throw RPN::InvalidArg("Wrong operation.");
 		}
-		i += 1;
 	}
-	std:: cout << std::endl;
-	for (std::deque<int>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
-		std::cout << "[" << *itr << "]" << std::flush;
-		if ((itr + 1) != buf.end()) {
-			std::cout << " -> " << std::flush;
+	catch(const std::exception& e) {
+		throw ;
+	}
+}
+
+static std::string	getToken(std::string& str, std::string::iterator& itr, const std::locale& l)
+{
+	std::string	token;
+
+	for (; !std::isspace(*itr, l); itr++) {
+		token += static_cast<char>(*itr);
+		if ((itr + 1) == str.end()) {
+			break ;
 		}
 	}
-	std:: cout << std::endl;
-}
-
-static void	multiplication(std::deque<int>& buf)
-{
-	int	a = buf.front();
-	buf.pop_front();
-	int	b = buf.front();
-	buf.pop_front();
-	int	result = a * b;
-	buf.push_front(result);
-}
-
-static void	division(std::deque<int>& buf)
-{
-	int	a = buf.front();
-	buf.pop_front();
-	int	b = buf.front();
-	buf.pop_front();
-	int	result = a / b;
-	buf.push_front(result);
-}
-
-static void	subtraction(std::deque<int>& buf)
-{
-	int	a = buf.front();
-	buf.pop_front();
-	int	b = buf.front();
-	buf.pop_front();
-	int	result = a - b;
-	buf.push_front(result);
-}
-
-static void	addition(std::deque<int>& buf)
-{
-	int	a = buf.front();
-	buf.pop_front();
-	int	b = buf.front();
-	buf.pop_front();
-	int	result = a + b;
-	buf.push_front(result);
+	if (token.size() != 1) {
+		throw RPN::InvalidArg("Only use 0 ~ 9 or +, -, *, /.");
+	}
+	return (token);
 }
 
 // SUBJECT FUNC
@@ -101,59 +140,61 @@ void	RPN::execute(std::string str) const
 		throw RPN::InvalidArg("Empty argument.");
 	}
 
-	std::deque<int>	buff;
-	std::locale		l = std::locale::classic();
-	for (std::string::iterator itr = str.begin(); itr != str.end(); itr++) {
-		if (!std::isspace(*itr, l)) {
-			std::string				token; 
-			// getToken(*itr, token);
-			std::string::iterator	tokenItr = itr;
-			for (; !std::isspace(*tokenItr, l); tokenItr++) {
-				token += static_cast<char>(*tokenItr);
-				if ((tokenItr + 1) == str.end()) {
-					break ;
-				}
+	std::deque<long>	buff;
+	std::locale			l = std::locale::classic();
+	try {
+		for (std::string::iterator itr = str.begin(); itr != str.end(); itr++) {
+			if (std::isspace(*itr, l)) {
+				continue ;
 			}
-			// validationToken(token)
-			if (token.size() != 1) {
-				throw RPN::InvalidArg("Only use 0 ~ 9.");
-			}
+
+			std::string	token = getToken(str, itr, l);
+
 			if (std::isdigit(static_cast<char>(token[0]), l)) {
 				buff.push_front(static_cast<char>(token[0]) - '0');
 			}
-			else if (this->operations_.find(token) != this->operations_.npos) {
-				if (buff.size() < 2) {
-					throw RPN::InvalidArg("Not enough numbers.");
-				}
-				// calculate(static_cast<char>(this->operations_.find(token)), buff);
-				switch (static_cast<char>(token[0])) {
-					case '+':
-						addition(buff);
-						break;
-					case '-':
-						subtraction(buff);
-						break;
-					case '/':
-						division(buff);
-						break;
-					case '*':
-						multiplication(buff);
-						break;
-					default:
-						throw RPN::InvalidArg("Wrong operation.");
-				}
+			else if (this->operations_.find(token[0]) != this->operations_.npos) {
+				calculate(static_cast<char>(token[0]), buff);
 			}
 			else {
 				throw RPN::InvalidArg("Non digit or operations(+-*/).");
 			}
-			itr = tokenItr;
+		}
+		if (buff.size() != 1) {
+			throw RPN::InvalidArg("Wrong format.");
 		}
 	}
-	if (buff.size() != 1) {
-		throw RPN::InvalidArg("Wrong format.");
+	catch (const std::exception& e) {
+		throw ;
 	}
-	printBuff(buff);
+	// printBuff(buff);
+	std::cout << buff.front() << std::endl;
 }
 
 // EXCEPTION
 RPN::InvalidArg::InvalidArg(const std::string& msg) : std::invalid_argument(msg) {}
+RPN::OverInt::OverInt(const std::string& msg) : std::out_of_range(msg) {}
+
+// debug
+// static void	printBuff(const std::deque<long>& buf)
+// {
+// 	if (buf.empty()) {
+// 		return ;
+// 	}
+// 	long	i = 0;
+// 	for (std::deque<long>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
+// 		std::cout << "[" << i << "]" << std::flush;
+// 		if ((itr + 1) != buf.end()) {
+// 			std::cout << " -> " << std::flush;
+// 		}
+// 		i += 1;
+// 	}
+// 	std:: cout << std::endl;
+// 	for (std::deque<long>::const_iterator itr = buf.begin(); itr != buf.end(); itr++) {
+// 		std::cout << "[" << *itr << "]" << std::flush;
+// 		if ((itr + 1) != buf.end()) {
+// 			std::cout << " -> " << std::flush;
+// 		}
+// 	}
+// 	std:: cout << std::endl;
+// }
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:03:14 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/11/09 15:56:41 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:41:25 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <locale>
 #include <deque>
+#include <algorithm>
 #include <stdexcept>
 
 class RPN {
@@ -34,6 +35,10 @@ public:
 	class InvalidArg : public std::invalid_argument {
 	public:
 		InvalidArg(const std::string& msg = "Invalid argument.");
+	};
+	class OverInt : public std::out_of_range {
+	public:
+		OverInt(const std::string& msg = "Over INT_MAX or under INT_MIN.");
 	};
 };
 
