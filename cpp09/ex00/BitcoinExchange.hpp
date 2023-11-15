8ee6d99c64b681ca20567df3e266628d5525f063
hnoguchi <>
2023-11-15 16:03:00 +0900

    [cpp09/ex00]BitcoinExchangeクラス。validationDate();の実装。

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hnoguchi <hnoguch@42tokyo.jp>                    +#+  +:+       +#+        */
+/*   By: hnoguchi <hnoguch@42tokyo.jp>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:51:17 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/11/14 18:39:17 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/11/15 15:21:55 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,43 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
 #include <map>
-#include "debugMessage.hpp"
+#include "color.hpp"
 
 class BitcoinExchange {
 private:
-	std::string		fileName_;
-	std::ifstream	fd_;
+	std::string							fileName_;
+	std::tm*							beginTime_;
+	// std::ifstream						fd_;
+	// std::map<std::string, std::string>	csvDate_;
 
 public:
 	// CONSTRUCTOR
-	BitcoinExchange(const std::string& name);
-	BitcoinExchange(const BitcoinExchange& src);
+	BitcoinExchange(const std::string& fileName = "data.csv");
+	// BitcoinExchange(const BitcoinExchange& src);
 	// DESTRUCTOR
 	~BitcoinExchange();
 	// OPERATOR
-	BitcoinExchange&	operator=(const BitcoinExchange& rhs);
+	// BitcoinExchange&	operator=(const BitcoinExchange& rhs);
 	// GETTER
-	const std::string&		getFileName() const;
-	const std::ifstream&	getFd() const;
+	// const std::string&		getFileName() const;
+	// const std::ifstream&	getFd() const;
 	// SETTER
+
+	// TODO: testが終わったらstaticに変更する。
+	void	parseDate(const std::string& date);
+	void	parseLine(const std::string& line);
+
+	// EXCEPTION
+	class FatalErr : public std::logic_error {
+	public:
+		FatalErr(const std::string& msg = "Fatal error.");
+	};
+	class ValidErr : public std::logic_error {
+	public:
+		ValidErr(const std::string& msg = "Validation error.");
+	};
 };
 
 #endif
