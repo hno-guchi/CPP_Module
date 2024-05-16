852d67bb58b77082953b89b75ce663d88dde0770
hnoguchi <>
2024-05-16 19:43:45 +0900

    [CPP09/ex02] PmergeMeクラスの実装を開始。

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -13,4 +13,36 @@ void				fatalError(const std::string& prefix, const std::string& message);
 void				printInt(const std::string& str, const std::vector<int>& vec);
 std::vector<int>	mergeInsertionSort(std::vector<int> vec);
 
+// class PmergeMe {
+//  private:
+// 	 std::vector<int>	before_;
+// 	 std::vector<int>	after_;
+// 	 int				cntPairComp_;
+// 	 int				cntRecvComp_;
+// 	 int				cntBinaryInsertComp_;
+// 	 time_t				time1_;
+// 	 time_t				time2_;
+// 
+// 	 std::vector<int>			mergeInsertionSort(const std::vector<int>& vec);
+// 	 void						printBefore() const;
+// 	 void						printAfter() const;
+// 	 void						printTime1() const;
+// 	 void						printTime2() const;
+// 	 void						printCntPair() const;
+// 	 void						printCntRecv() const;
+// 	 void						printCntBinaryInsert() const;
+// 
+//  public:
+// 	 PmergeMe();
+// 	 ~PmergeMe();
+// 	 void						setBefore(int argc, char** argv);
+// 	 void						setAfter();
+// 	 const std::vector<int>&	getBefore() const;
+// 	 const std::vector<int>&	getAfter() const;
+// 
+// 	 // DEBUG
+// 	 void						print() const;
+// 	 void						printCnt() const;
+// }
+
 # endif  // PMERGEME_HPP
